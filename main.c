2e5a08977ace8eb0d81d918d856b88b14e81df67
Victor Lavrentev <victor.lavrentev@gmail.com>
2015-09-11 16:50:38 +0300

    edit main.c
    main.c

--- main.c
+++ main.c
@@ -1,44 +1,16 @@
 #include<stdio.h>
 #include <string.h>
-#include "table.h"
+#include <stdlib.h>
+#include "vxlib.h"
 
-int main()
+int main(void)
 {
-   struct table Table1;
-    
-   strcpy( Table1.scene, "Forest\n");
-   strcpy( Table1.shot, "shot 01\n"); 
-   strcpy( Table1.data, "Retouch wires\n");
-   strcpy( Table1.source, "-scene-shot01-data.exr\n");
-   strcpy( Table1.id, "001");
-   
-   FILE *fp;
-
-   fp = fopen("data.txt", "w+");
-   fprintf(fp, Table1.scene);
-   fprintf(fp, Table1.shot);
-   fprintf(fp, Table1.data);
-   fprintf(fp, Table1.source);
-   fprintf(fp, Table1.id);
-   fclose(fp);
-   
-   printf("write\n");
-   
-   char buff[255];
-
-   fp = fopen("data.txt", "r");
-   int i=1;
-   char num[12];
-   while(!feof(fp)){ 
-     fgets(buff, 255, (FILE*)fp);
-     sprintf(num, "%u", i );
-     printf(num);
-     printf(": %s\n", buff );
-     i=i+1;
-   };
-   
-   //fgets(buff, 255, (FILE*)fp);
-   //printf("2: %s\n", buff );
-   fclose(fp);
-   printf("\nread\n");
-}
+    struct TLine _line;
+    //wrile_line("Forest\n", "shot 01\n", "Retouch wires\n",  "-scene-shot01-data.exr\n", "001\n");
+    //wrile_line("City\n", "shot 02\n", "Retouch wires\n",  "-scene-shot02-data.exr\n", "002\n");
+    //wrile_line("River\n", "shot 03\n", "Retouch wires\n",  "-scene-shot03-data.exr\n", "003\n");
+    printf("->Load data\n\n");
+    read_line();
+    printf("\n->End\n");
+    return 0;
+};
