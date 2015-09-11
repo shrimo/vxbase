3ca5e724ee0c1b9120140ca7bbb9cd6e8f16a3dd
Victor Lavrentev <victor.lavrentev@gmail.com>
2015-09-12 02:40:59 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,17 +5,23 @@
 
 int main(int argc, char *argv[])
 {
-    struct TLine _line;
-    //wrile_line("Forest\n", "shot_01\n", "Retouch wires\n",  "-scene-shot01-data.exr\n", "001\n");
-    //wrile_line("City\n", "shot_02\n", "Retouch wires\n",  "-scene-shot02-data.exr\n", "002\n");
-    //wrile_line("River\n", "shot_03\n", "Retouch wires\n",  "-scene-shot03-data.exr\n", "003\n");
+    printf ("VXDatabase 0.01\n");
+        if(argc!=3) {
+                printf("No request\n");
+                exit(1);
+        }
+        struct TLine _line;
+        //wrile_line("Forest\n", "shot_01\n", "Retouch wires\n",  "-scene-shot01-data.exr\n", "001\n");
+        //wrile_line("City\n", "shot_02\n", "Retouch wires\n",  "-scene-shot02-data.exr\n", "002\n");
+        //wrile_line("River\n", "shot_03\n", "Retouch wires\n",  "-scene-shot03-data.exr\n", "003\n");
     
-    //printf("->Load data\n\n");
-    //read_line("data.txt");
+        //printf("->Load data\n\n");
+        //read_line("data.txt");
     
-    printf("\n->Search line\n");
-    search_line("data.txt",argv[1]);
+        printf("\n->Search line\n");
+
+        search_line("data.txt",argv[1], atoi(argv[2]));
     
-    printf("\n->End\n");
-    return 0;
+        printf("\n->End\n");
+        return 0;
 };
