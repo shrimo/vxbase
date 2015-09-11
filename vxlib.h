414a377fb235d7d968f309c5bc538537346ee9c5
Victor Lavrentev <victor.lavrentev@gmail.com>
2015-09-12 00:38:43 +0300

    add search
    vxbase.h

--- vxlib.h
+++ vxlib.h
@@ -19,13 +19,57 @@ void wrile_line(char scene[255], char shot[255], char data[255], char source[255
    fclose(fp);
 };
 
-void read_line()
+void read_line(char *fname)
 {
    struct TLine line;
    FILE *fr;
 
-   fr = fopen("data.txt", "r");
+   fr = fopen(fname, "r");
    while (fread (&line, sizeof(struct TLine), 1, fr))
    fclose(fr);
    printf(line.scene, line.shot, line.data, line.source, line.id);   
 };
+
+int search_line (char *fname, char *str)
+{
+        struct TLine line[255];
+        FILE *fe;
+	
+        if((fe = fopen(fname, "r")) == NULL) 
+        {
+                return(-1);
+        }
+        fe = fopen("data.txt", "r");
+        int i=0;        
+        char num[12];
+        while(!feof(fe))
+        {     
+                fgets(line[i].scene, 255, (FILE*)fe);
+                fgets(line[i].shot, 255, (FILE*)fe);
+                fgets(line[i].data, 255, (FILE*)fe);
+                fgets(line[i].source, 255, (FILE*)fe);
+                fgets(line[i].id, 255, (FILE*)fe);
+                sprintf(num, "%u", i );
+                //printf(num);
+                //printf(": %s", line[i].scene );
+                i=i+1;
+        }        
+        fclose(fe);
+        
+        i=0;        
+        while (strlen(line[i].shot) != 0 )
+        {
+                if((strstr(line[i].shot, str)) != NULL) 
+                {
+                        printf("A match found on line: %d\n", i);
+                        printf(" %s %s %s %s %s",line[i].scene, line[i].shot, line[i].data, line[i].source, line[i].id);
+                }
+                i++;
+        }
+        return(0);
+
+        if(i == 0) 
+        {
+                printf("\nSorry, couldn't find a match.\n");
+        }
+}
