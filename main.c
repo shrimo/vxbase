f03cbbd6da8052997f87221361e7ea5b15d95c51
Victor Lavrentev <victor.lavrentev@gmail.com>
2015-09-23 04:29:32 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include "vxcore_lib.h"
 #include "vxscripts.h"
+#include "vxcore_lib.h"
 
 int main(int argc, char *argv[]) 
 {
     printf("VX database core v 0.01\n");
-    /*if(argc!=3) {
+    if(argc!=3) {
                 printf("No command-line arguments\n");
                 exit(1);
         }
@@ -21,18 +16,20 @@ int main(int argc, char *argv[])
     };
     if((strstr(argv[1], "-f")) != NULL){
             printf("Read scrpt file ->%s \n", argv[2]);
-            scripts(argv[2]);
-    };*/
+            scripts(argv[2],"root");
+    };
     //printf ("%s %s\n\n", argv[1], argv[2]);
-    v_add("variable_01\n", "root\n", "vchar\n","text 01\n");
+    /*v_add("variable_01\n", "root\n", "vchar\n","text 01\n");
     v_add("variable_02\n", "user\n", "vchar\n","text 02\n");
     v_add("variable_03\n", "guest\n", "vchar\n","text 03\n");
     v_add("variable_04\n", "root\n", "vchar\n","text 04\n");
     v_add("variable_05\n", "user\n", "vchar\n","text 05\n");
+    v_add("variable_06\n", "test\n", "vchar\n","text 06\n");
+    v_add("variable_07\n", "root\n", "vchar\n","text 07\n");*/
     
     //v_read("data.db");
     //v_print(1);
     
-    v_write("data.db");
+    //v_write("data.db");
     return 0;
 };
